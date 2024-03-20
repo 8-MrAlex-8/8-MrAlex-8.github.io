c3c13dde89e67587dd0ed62672be65f880f72510
LoMolts-382 <lanzimalto@su.edu.ph>
2024-03-20 17:29:09 +0800

    last commit for the afternoon

---
+++ Untitled-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head> Sample PHP </head>
+
+<body>
+
+    <h1> My first PHP page </h1>
+
+    <?php
+    echo "<h3> Hello World! </h3>";
+    print "<p> Comparing echo and print </p>";
+    ?>
+
+</body>
+
+</html>
